ac49bffba7644a511db68247bbe34dff27e14ad5
Panitnun Suvannabun <Panitnun.6243@gmail.com>
2022-06-14 01:17:12 +0700

    Create navigation service

--- lib/pages/splash_page.dart
+++ lib/pages/splash_page.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 class SplashPage extends StatefulWidget {
@@ -16,6 +17,9 @@ class _SplashPageState extends State<SplashPage> {
   @override
   void initState() {
     super.initState();
+    _setup().then(
+      (_) => widget.onInitializationComplete(),
+    );
   }
 
   @override
@@ -24,15 +28,15 @@ class _SplashPageState extends State<SplashPage> {
       debugShowCheckedModeBanner: false,
       title: 'CShat',
       theme: ThemeData(
-        backgroundColor: Color.fromRGBO(36, 35, 49, 1.0),
-        scaffoldBackgroundColor: Color.fromRGBO(36, 35, 49, 1.0),
+        backgroundColor: const Color.fromRGBO(36, 35, 49, 1.0),
+        scaffoldBackgroundColor: const Color.fromRGBO(36, 35, 49, 1.0),
       ),
       home: Scaffold(
           body: Center(
         child: Container(
           width: 200,
           height: 200,
-          decoration: BoxDecoration(
+          decoration: const BoxDecoration(
               image: DecorationImage(
                   fit: BoxFit.contain,
                   image: AssetImage('assets/images/logo.png'))),
@@ -40,4 +44,11 @@ class _SplashPageState extends State<SplashPage> {
       )),
     );
   }
+
+  Future<void> _setup() async {
+    WidgetsFlutterBinding.ensureInitialized();
+    await Firebase.initializeApp();
+  }
+
+  void _registerServices() {}
 }
